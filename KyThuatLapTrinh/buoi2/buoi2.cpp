77c5be8a5d076aad8740700763616546629413f1
PMT201 <PMT201@PMT3028>
2026-04-03 08:21:13 +0700

    buoi2

--- KyThuatLapTrinh/buoi2/buoi2.cpp
+++ KyThuatLapTrinh/buoi2/buoi2.cpp
@@ -25,6 +25,7 @@ struct Person {
 struct Node {
 	Person data;
 	Node* next;
+	Node(Person x) : data(x), next (nullptr) {}
 };
 
 struct LinkedList {
@@ -41,78 +42,106 @@ struct LinkedList {
 			item = item->next;
 		}
 	}
+	void Add(Person x) {
+		Node* newNode = new Node(x);
+		newNode->next = head;
+		head = newNode;
+	}
+	bool Remove(int x) {
+		if (head == NULL) {
+			return false;
+		}
+		Node* item = head;
+		if (item->data.id == x) {
+			head = item->next;
+			delete item;
+			return true;
+		}
+		while (item->next != NULL) {
+			if (item->next->data.id == x) {
+				Node* temp = item->next;
+				item->next = temp->next;
+				delete temp;
+				return true;
+			}
+			item = item->next;
+		}
+	}
 };
 
-int main()
-{
-	LinkedList list = {NULL};
-	do {
-		system("cls");
-		cout << "--- HUMAN RESOURCE --------" << endl;
-		cout << "1. View person list" << endl;
-		cout << "2. Add a person" << endl;
-		cout << "3. Remove a person" << endl;
-		cout << "4. Find a person by name" << endl;
-		cout << "5. Export to file " << endl;
-		cout << "6. Import from file" << endl;
-		cout << "0. Exit" << endl;
-		cout << "-----------------------------" << endl;
-		cout << "Your command: ";
-		int cmd;
-		cin >> cmd;
-		switch (cmd)
-		{
-		case 1: {
-			
-			break;
-		}
-		case 2: {
-			
-			break;
-		}
-		case 3: {
-			int id;
-			cout << "Input ID to remove: ";
-			cin >> id;
-			
-			break;
-		}
-		case 4: {
-			string name;
-			cout << "Input Name to remove: ";
-			cin.ignore();
-			getline(cin, name);
-			bool res = false;
-			if (res) {
-				cout << "Found person with name" << name << endl;
+
+
+	int main()
+	{
+		LinkedList list = { NULL };
+		do {
+			system("cls");
+			cout << "--- HUMAN RESOURCE --------" << endl;
+			cout << "1. View person list" << endl;
+			cout << "2. Add a person" << endl;
+			cout << "3. Remove a person" << endl;
+			cout << "4. Find a person by name" << endl;
+			cout << "5. Export to file " << endl;
+			cout << "6. Import from file" << endl;
+			cout << "0. Exit" << endl;
+			cout << "-----------------------------" << endl;
+			cout << "Your command: ";
+			int cmd;
+			cin >> cmd;
+			switch (cmd)
+			{
+			case 1: {
+
+				break;
 			}
-			else
-				cout << "Person isn't existed" << endl;
+			case 2: {
+
+				break;
+			}
+			case 3: {
+				int id;
+				cout << "Input ID to remove: ";
+				cin >> id;
+				bool res = list.Remove(id);
+				if (res)
+					cout << "Remove a person successfully" << endl;
+				else
+					cout << "Person isn't found" << endl;
+				break;
+			}
+			case 4: {
+				string name;
+				cout << "Input Name to remove: ";
+				cin.ignore();
+				getline(cin, name);
+				bool res = false;
+				if (res) {
+					cout << "Found person with name" << name << endl;
+				}
+				else
+					cout << "Person isn't existed" << endl;
 
 
-			break;
-		}
-		case 5: {
-			cout << "Export successfully" << endl;
-			break;
-		}
-		case 6: {
-			cout << "Import successfully" << endl;
-			break;
-		}
-		case 0: {
-			return 0;
-		}
-		default:
-			cout << "Your command isn't found. Try again..." << endl;
-			break;
-		}
-		cout << "Press enter to continue...";
-		cin.ignore();
-		cin.get();
-	} while (true);
-	return 0;
-
-}
-
-
+				break;
+			}
+			case 5: {
+				cout << "Export successfully" << endl;
+				break;
+			}
+			case 6: {
+				cout << "Import successfully" << endl;
+				break;
+			}
+			case 0: {
+				return 0;
+			}
+			default:
+				cout << "Your command isn't found. Try again..." << endl;
+				break;
+			}
+			cout << "Press enter to continue...";
+			cin.ignore();
+			cin.get();
+		} while (true);
+		return 0;
+	}
